1bc77dfa4917c256f6ee4423031d0d58c383b2df
chd <chd12190@163.com>
2016-12-29 20:11:15 +0800

    初始化

---
+++ Server.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fleck.online
+{
+    class Server
+    {
+        static void Main()
+        {
+            FleckLog.Level = LogLevel.Debug;
+            var allSockets = new List<IWebSocketConnection>();
+            var server = new WebSocketServer("ws://0.0.0.0:8183");
+            RoomSet roomSet = new RoomSet();
+
+            server.Start(socket =>
+                {
+                    socket.OnOpen = () =>
+                        {
+                            Console.WriteLine("Open!");
+                            allSockets.Add(socket);  
+                        };
+                    socket.OnClose = () =>
+                        {
+                            Console.WriteLine(socket.ConnectionInfo.ClientIpAddress + ":"+ socket.ConnectionInfo.ClientPort.ToString() + " Close!");
+                            allSockets.Remove(socket);
+                            roomSet.Remove(socket);
+                        };
+                    socket.OnMessage = message =>
+                        {
+                            if (message.IndexOf("roomid") != -1)
+                            {
+                                roomSet.Add(message, socket);                                
+                            }
+                            else if (message.IndexOf("move") != -1)
+                            {
+                                roomSet.Send(socket, message);
+                            }
+                            else if (message.IndexOf("resign") != -1)
+                            {
+                                roomSet.RemoveAll(socket);
+                            }                             
+                                          
+                            Console.WriteLine(message);
+                            
+                        };
+                });
+            
+            var input = Console.ReadLine();
+            while (input != "exit")
+            {
+                foreach (var socket in allSockets.ToList())
+                {
+                    socket.Send(input);
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }  
+}
